f21d5ce1224df8c6b24e3cd1bc2f181b4a0da80e
planet36 <planet36@users.noreply.github.com>
2020-06-11 19:33:12 -0400

    Remove allowoblique
    Improve the order that patterns are deleted

--- x.c
+++ x.c
@@ -1003,20 +1003,18 @@ xloadfonts(char *fontstr, double fontsize)
 	win.ch = ceilf(dc.font.height * chscale);
 
 	FcPatternDel(pattern, FC_SLANT);
+	FcPatternDel(pattern, FC_WEIGHT);
 	if (allowitalic)
 		FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ITALIC);
 	if (xloadfont(&dc.ifont, pattern))
 		die("can't open font %s\n", fontstr);
 
-	FcPatternDel(pattern, FC_WEIGHT);
 	if (allowbold)
 		FcPatternAddInteger(pattern, FC_WEIGHT, FC_WEIGHT_BOLD);
 	if (xloadfont(&dc.ibfont, pattern))
 		die("can't open font %s\n", fontstr);
 
 	FcPatternDel(pattern, FC_SLANT);
-	if (allowoblique)
-		FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ROMAN);
 	if (xloadfont(&dc.bfont, pattern))
 		die("can't open font %s\n", fontstr);
 
