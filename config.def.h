4f67a0a05c7e8c26481c45d308ecb8d26aee25d7
planet36 <planet36@users.noreply.github.com>
2020-06-27 16:35:55 -0400

    Change font to monospace

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see https://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Go Mono:size=10:antialias=true:autohint=true";
+static char *font = "monospace:size=10:antialias=true:autohint=true";
 
 static int allowbold = 0;
 static int allowitalic = 1;
