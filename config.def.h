45597ad36936182977700d7e4cf1ed585918c3ee
planet36 <planet36@users.noreply.github.com>
2021-01-17 00:10:28 -0500

    Change font size to 12

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see https://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=10:antialias=true:autohint=true";
+static char *font = "monospace:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
