bd34db8adbe9e39f18c0a63f6112d8f37a6050c8
planet36 <planet36@users.noreply.github.com>
2021-02-28 23:09:07 -0500

    Use actual int div ceil code

--- st.h
+++ st.h
@@ -1,5 +1,6 @@
 /* See LICENSE for license details. */
 
+#include <math.h>
 #include <stdint.h>
 #include <sys/types.h>
 
@@ -8,7 +9,7 @@
 #define MAX(a, b)		((a) < (b) ? (b) : (a))
 #define LEN(a)			(sizeof(a) / sizeof(a)[0])
 #define BETWEEN(x, a, b)	((a) <= (x) && (x) <= (b))
-#define DIVCEIL(n, d)		(((n) + ((d) - 1)) / (d))
+#define DIVCEIL(n, d)		ceil((double)(n) / (double)(d))
 #define DEFAULT(a, b)		(a) = (a) ? (a) : (b)
 #define LIMIT(x, a, b)		(x) = (x) < (a) ? (a) : (x) > (b) ? (b) : (x)
 #define ATTRCMP(a, b)		((a).mode != (b).mode || (a).fg != (b).fg || \
