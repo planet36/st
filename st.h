6dd2cc3bf249383d40357e427b12108f7d7075ef
planet36 <planet36@users.noreply.github.com>
2021-04-13 12:42:11 -0400

    Use parentheses with sizeof operator

--- st.h
+++ st.h
@@ -7,7 +7,7 @@
 /* macros */
 #define MIN(a, b)		((a) < (b) ? (a) : (b))
 #define MAX(a, b)		((a) < (b) ? (b) : (a))
-#define LEN(a)			(sizeof(a) / sizeof(a)[0])
+#define LEN(a)			(sizeof(a) / sizeof((a)[0]))
 #define BETWEEN(x, a, b)	((a) <= (x) && (x) <= (b))
 #define DEFAULT(a, b)		(a) = (a) ? (a) : (b)
 #define LIMIT(x, a, b)		(x) = (x) < (a) ? (a) : (x) > (b) ? (b) : (x)
