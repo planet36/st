4cb44ffa21641aee6c64169345fc5cc1725680d3
planet36 <planet36@users.noreply.github.com>
2021-05-04 16:22:10 -0400

    Make MIN preserve order

--- st.h
+++ st.h
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 
 /* macros */
-#define MIN(a, b)		((a) < (b) ? (a) : (b))
+#define MIN(a, b)		((b) < (a) ? (b) : (a))
 #define MAX(a, b)		((a) < (b) ? (b) : (a))
 #define LEN(a)			(sizeof(a) / sizeof((a)[0]))
 #define BETWEEN(x, a, b)	((a) <= (x) && (x) <= (b))
