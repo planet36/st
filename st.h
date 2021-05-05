15d0815fd90df4e89eeec3cb5ee592e33be5f484
planet36 <planet36@users.noreply.github.com>
2021-05-05 10:21:59 -0400

    Follow the suckless coding style
    https://suckless.org/coding_style/

--- st.h
+++ st.h
@@ -5,20 +5,20 @@
 #include <sys/types.h>
 
 /* macros */
-#define MIN(a, b)		((b) < (a) ? (b) : (a))
-#define MAX(a, b)		((a) < (b) ? (b) : (a))
-#define LEN(a)			(sizeof(a) / sizeof((a)[0]))
-#define BETWEEN(x, a, b)	((a) <= (x) && (x) <= (b))
-#define DEFAULT(a, b)		(a) = (a) ? (a) : (b)
-#define LIMIT(x, a, b)		(x) = (x) < (a) ? (a) : (x) > (b) ? (b) : (x)
-#define ATTRCMP(a, b)		((a).mode != (b).mode || (a).fg != (b).fg || \
-				(a).bg != (b).bg)
-#define TIMEDIFF(t1, t2)	(((t1).tv_sec-(t2).tv_sec)*1000 + \
-				((t1).tv_nsec-(t2).tv_nsec)/1E6)
-#define MODBIT(x, set, bit)	((set) ? ((x) |= (bit)) : ((x) &= ~(bit)))
+#define MIN(a, b)               ((b) < (a) ? (b) : (a))
+#define MAX(a, b)               ((a) < (b) ? (b) : (a))
+#define LEN(a)                  (sizeof(a) / sizeof((a)[0]))
+#define BETWEEN(x, a, b)        ((a) <= (x) && (x) <= (b))
+#define DEFAULT(a, b)           (a) = (a) ? (a) : (b)
+#define LIMIT(x, a, b)          (x) = (x) < (a) ? (a) : (x) > (b) ? (b) : (x)
+#define ATTRCMP(a, b)           ((a).mode != (b).mode || (a).fg != (b).fg || \
+                                (a).bg != (b).bg)
+#define TIMEDIFF(t1, t2)        (((t1).tv_sec-(t2).tv_sec)*1000 + \
+                                ((t1).tv_nsec-(t2).tv_nsec)/1E6)
+#define MODBIT(x, set, bit)     ((set) ? ((x) |= (bit)) : ((x) &= ~(bit)))
 
-#define TRUECOLOR(r,g,b)	(1 << 24 | (r) << 16 | (g) << 8 | (b))
-#define IS_TRUECOL(x)		(1 << 24 & (x))
+#define TRUECOLOR(r,g,b)        (1 << 24 | (r) << 16 | (g) << 8 | (b))
+#define IS_TRUECOL(x)           (1 << 24 & (x))
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
