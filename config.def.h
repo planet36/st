0b6e93d638c7a4d785e8f207932dbd0e415882df
planet36 <planet36@users.noreply.github.com>
2022-01-06 16:25:31 -0500

    Use srcery colors for foreground, background, cursor, reverse cursor
    https://github.com/srcery-colors/srcery-terminal/blob/master/st/srcery_st.h

--- config.def.h
+++ config.def.h
@@ -119,6 +119,9 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with defaultXX */
+	"#fce8c3", /* foreground */
+	"#1c1b19", /* background */
+	"#fbb829", /* cursor */
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
@@ -129,9 +132,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
