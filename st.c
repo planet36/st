2655a9703ff38d05c0826cb7cece709be76ad365
planet36 <planet36@users.noreply.github.com>
2022-04-19 07:06:06 -0500

    Follow the suckless coding style
    https://suckless.org/coding_style/

--- st.c
+++ st.c
@@ -1969,7 +1969,7 @@ osc_color_response(int num, int index, int is_osc4)
 		return;
 	}
 
-	n = snprintf(buf, sizeof buf, "\033]%s%d;rgb:%02x%02x/%02x%02x/%02x%02x\007",
+	n = snprintf(buf, sizeof(buf), "\033]%s%d;rgb:%02x%02x/%02x%02x/%02x%02x\007",
 	             is_osc4 ? "4;" : "", num, r, r, g, g, b, b);
 	if (n < 0 || n >= sizeof(buf)) {
 		fprintf(stderr, "error: %s while printing %s response\n",
